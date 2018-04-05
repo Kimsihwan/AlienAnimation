ec6faf46f01305104b2eb05664510bdc5d905b30
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-05 15:35:21 +0900

    Alien Animation Start/Stop

--- AlienAnimation/ViewController.swift
+++ AlienAnimation/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var lbl: UILabel!
+    var chk = true
+    var counter = 1
+    var myTimer = Timer()
+    var isAnimating = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    lbl.text = String(counter)
     }
+    
+    @IBAction func start(_ sender: Any) {
+        if isAnimating == false {
+                    myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.doAniation), userInfo: nil, repeats: true)
+            isAnimating = true
+        } else {
+            myTimer.invalidate()
+            isAnimating = false
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @objc func doAniation(){
+        if counter == 5 {
+            chk = false // 내리막
+            counter = counter - 1
+        } else if counter == 1 {
+            chk = true // 오르막
+            counter = counter + 1
+        } else if chk == false {
+            counter = counter - 1
+        } else if chk == true {
+            counter = counter + 1
+        }
+        img.image = UIImage(named: "frame\(counter).png")
+        lbl.text = String(counter)
+    }
+    
 }
 
